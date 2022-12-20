086461919799caf9a773fad908e88a0b3cb20dbf
Scot Walters <scowalt@gmail.com>
2022-12-20 12:14:33 -0800

    Add version to telemetry

--- scripts/track.ps1
+++ scripts/track.ps1
@@ -3,7 +3,9 @@ param (
     [Parameter(Mandatory = $true)]
     [string]$eventName,
     [Parameter(Mandatory = $true)]
-    [string]$app
+    [string]$app,
+    [Parameter(Mandatory = $true)]
+    [string]$version
 )
 
 $token = "6d8fab63250e36199e559a247a72f87f"
@@ -14,5 +16,5 @@ Invoke-WebRequest `
     -Uri "https://api.mixpanel.com/track" `
     -Headers $headers `
     -TimeoutSec 5 `
-    -Body "[{ ""event"": ""$eventName"", ""properties"": { ""time"": $([int64](Get-Date(Get-Date).ToUniversalTime() -UFormat %s)) , ""token"": ""$token"", ""distinct_id"": ""$((Get-WmiObject -Class Win32_ComputerSystemProduct).UUID)"", ""app"": ""$app"", ""`$insert_id"": ""$(New-GUID)"" } }]" `
-    | Out-Null
+    -Body "[{ ""event"": ""$eventName"", ""properties"": { ""time"": $([int64](Get-Date(Get-Date).ToUniversalTime() -UFormat %s)) , ""token"": ""$token"", ""distinct_id"": ""$((Get-WmiObject -Class Win32_ComputerSystemProduct).UUID)"", ""app"": ""$app"", ""version"": ""$version"", ""`$insert_id"": ""$(New-GUID)"" } }]" `
+| Out-Null
